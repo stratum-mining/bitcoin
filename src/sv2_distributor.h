b130679e212020918f99731ba1dbd5a296559d80
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-11-29 15:41:31 +0000

    cache the changes for the sv2 distributor class

--- src/sv2_distributor.h
+++ src/sv2_distributor.h
@@ -7,20 +7,26 @@
 #include <rusty/protocols/v2/sv2-ffi/sv2.h>
 
 CNewTemplate AssembleSv2BlockTemplate(CChainState& chainstate, const CTxMemPool& mempool, const CChainParams& params, const BlockAssembler::Options options) {
-    auto block_template = BlockAssembler(chainstate, mempool, params, options).CreateNewBlock(CScript());
+    std::unique_ptr<CBlockTemplate> pblocktemplate = BlockAssembler(chainstate, mempool, params, options)
+        .CreateNewBlock(CScript());
 
-    const CBlockHeader header = block_template->block.GetBlockHeader();
-
+    const CBlock block = pblocktemplate->block;
+    const CBlockHeader header = block.GetBlockHeader();
+    
     CNewTemplate ctemplate;
     ctemplate.template_id = GetTimeSeconds();
+
+    // TODO: Decide when this is a future block or not.
     ctemplate.future_template = false;
     ctemplate.version = header.nVersion;
 
-    const auto coinbase_tx = block_template->block.vtx[0];
+    const auto coinbase_tx = block.vtx[0];
     ctemplate.coinbase_tx_version = coinbase_tx->CURRENT_VERSION;
 
     CDataStream coinbase_script(SER_NETWORK, PROTOCOL_VERSION);
     coinbase_script << coinbase_tx->vin[0].scriptSig;
+
+    // TODO: Double check why 8 is hardcoded as the length?
     ctemplate.coinbase_prefix = cvec_from_buffer(&coinbase_script[0], 8);
     ctemplate.coinbase_tx_input_sequence = coinbase_tx->vin[0].nSequence;
 
@@ -33,13 +39,16 @@ CNewTemplate AssembleSv2BlockTemplate(CChainState& chainstate, const CTxMemPool&
     ctemplate.coinbase_tx_outputs = cvec_from_buffer(&vout[0], vout.size());
     ctemplate.coinbase_tx_locktime = coinbase_tx->nLockTime;
 
-    CDataStream merkle_path_stream(SER_NETWORK, PROTOCOL_VERSION);
-    for (const auto& tx: block_template->block.vtx) {
+    CVec2 cvec2 = init_cvec2();
+    for (const auto& tx: block.vtx) {
+        CDataStream merkle_path_stream(SER_NETWORK, PROTOCOL_VERSION);
         merkle_path_stream << tx->GetHash();
+
+        auto merkle_path = cvec_from_buffer(&merkle_path_stream[0], merkle_path_stream.size());
+        cvec2_push(&cvec2, merkle_path);
     }
 
-    CVec cvec_merkle_path = cvec_from_buffer(&merkle_path_stream[0], merkle_path_stream.size());
-    ctemplate.merkle_path = CVec2{&cvec_merkle_path, 1, 1};
+    ctemplate.merkle_path = cvec2;
 
     return ctemplate;
 }
