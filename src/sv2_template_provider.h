a4037da024343d0259d78a095db67efabe5c910b
ccdle12 <chris.coverdale24@gmail.com>
2023-03-28 05:06:27 +0000

    sv2: fix non-conformity to specs for NewTemplate serialization

--- src/sv2_template_provider.h
+++ src/sv2_template_provider.h
@@ -304,35 +304,7 @@ public:
 
     NewTemplateMsg() = default;
 
-    explicit NewTemplateMsg(const CBlock& block, uint64_t template_id, bool future_template)
-        : m_template_id{template_id}, m_future_template{future_template}
-    {
-        m_version = block.GetBlockHeader().nVersion;
-
-        const CTransactionRef coinbase_tx = block.vtx[0];
-        m_coinbase_tx_version = coinbase_tx->CURRENT_VERSION;
-        m_coinbase_prefix = coinbase_tx->vin[0].scriptSig;
-        m_coinbase_tx_input_sequence = coinbase_tx->vin[0].nSequence;
-
-        // The coinbase nValue already contains the nFee + the Block Subsidy when built using CreateBlock().
-        m_coinbase_tx_value_remaining = static_cast<uint64_t>(block.vtx[0]->vout[0].nValue);
-
-        m_coinbase_tx_outputs_count = 0;
-        int commitpos = GetWitnessCommitmentIndex(block);
-        if (commitpos != NO_WITNESS_COMMITMENT) {
-            m_coinbase_tx_outputs_count = 1;
-            std::vector<CTxOut> coinbase_tx_outputs{block.vtx[0]->vout[commitpos]};
-            m_coinbase_tx_outputs = coinbase_tx_outputs;
-        }
-
-        m_coinbase_tx_locktime = coinbase_tx->nLockTime;
-
-        // Skip the coinbase_tx hash from the merkle path, as the downstream client
-        // will build their own coinbase tx.
-        for (auto it = block.vtx.begin() + 1; it != block.vtx.end(); ++it) {
-            m_merkle_path.push_back((*it)->GetHash());
-        }
-    };
+    explicit NewTemplateMsg(const CBlock& block, uint64_t template_id, bool future_template);
 
     template <typename Stream>
     void Serialize(Stream& s) const
@@ -343,19 +315,22 @@ public:
           << m_coinbase_tx_version
           << m_coinbase_prefix
           << m_coinbase_tx_input_sequence
-          << m_coinbase_tx_value_remaining;
-        if (m_coinbase_tx_outputs_count > 0) {
-            s << static_cast<uint32_t>(m_coinbase_tx_outputs_count);
+          << m_coinbase_tx_value_remaining
+          << m_coinbase_tx_outputs_count;
 
+        // If there are more than 0 coinbase tx outputs, then we need to serialize them
+        // as B0_64K.
+        if (m_coinbase_tx_outputs_count > 0) {
             std::vector<uint8_t> outputs_bytes;
             CVectorWriter{SER_NETWORK, PROTOCOL_VERSION, outputs_bytes, 0, m_coinbase_tx_outputs[0]};
 
             s << static_cast<uint16_t>(outputs_bytes.size());
             s.write(MakeByteSpan(outputs_bytes));
         } else {
-            // We still need to send 2 bytes indicating an empty coinbase-tx_outputs array. */
+            // We will still need to send 2 bytes indicating an empty coinbase-tx_outputs array as a B0_64K.
             s << static_cast<uint16_t>(0);
         }
+
         s << m_coinbase_tx_locktime
           << m_merkle_path;
     }
